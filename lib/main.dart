50eec97688806972bbf128f4e1ac75c5e6bed98d
nb312 <nbfly01@163.com>
2019-01-09 18:45:25 +0800

    delete all code before and add the animation example.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Layout Higher',
+      title: 'Layout Animation',
       home: WelcomePage(),
       routes: {},
     );
